aea9c8a7f9c75a319c3bd347d04232476d508d9c
xlz122 <122874722@qq.com>
2022-05-05 16:12:04 +0800

    feat: 完成底部菜单导航

---
+++ src/pages/drink/Drink.vue
@@ -0,0 +1,11 @@
+<template>
+  <view>点餐</view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'DrinkView'
+});
+</script>
---
+++ src/pages/take-foods/TakeFoods.vue
@@ -0,0 +1,11 @@
+<template>
+  <view>取餐</view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TakeFoods'
+});
+</script>
