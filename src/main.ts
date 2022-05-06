906fc612b9c31f779506f5d74e459e07b44c66ca
xlz122 <122874722@qq.com>
2022-05-06 10:07:35 +0800

    feat: 完成奈雪乐园页面

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
+import './index.scss';
 
 export function createApp() {
   const app = createSSRApp(App);
+
   return {
     app
   };
---
+++ src/pages/login/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <view>登录</view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginView'
+});
+</script>
